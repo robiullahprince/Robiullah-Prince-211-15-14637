79ce3f55e8cbf5a3ab27b430d508d55d95b95f16
Robiullah Prince <prince15-14637@diu.edu.bd>
2022-02-22 11:09:56 +0600

    adding

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,8 +1,9 @@
-#include<stdio.h>
-int main(){
-    int a ;
+#include <stdio.h>
+int main()
+{
+    int a;
     printf("hello\n");
-    scanf("%d",&a);
-    printf("%d",a);
-    return 0 ;
+    scanf("%d", &a);
+    printf("%d", a);
+    return 0;
 }
